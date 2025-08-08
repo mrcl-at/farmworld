1f150f9ab9b5ae9498e7cabaf09b1216e4079c02
mrcl <hey@mrcl.at>
2025-08-08 04:40:28 +0200

    Move farm world loading logic from `FarmWorldBootstrapper` to `FarmWorldPlugin`, improve plugin structure and responsibility separation

--- paper-plugin/src/main/java/at/mrcl/farmworld/FarmWorldBootstrapper.java
+++ paper-plugin/src/main/java/at/mrcl/farmworld/FarmWorldBootstrapper.java
@@ -3,12 +3,6 @@ package at.mrcl.farmworld;
 import at.mrcl.farmworld.api.Bootstrapper;
 import at.mrcl.farmworld.api.FarmWorldAPI;
 import at.mrcl.farmworld.database.SQLiteDatabase;
-import org.bukkit.World;
-import org.bukkit.plugin.java.JavaPlugin;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.CompletableFuture;
 
 public class FarmWorldBootstrapper extends Bootstrapper {
 
@@ -20,8 +14,6 @@ public class FarmWorldBootstrapper extends Bootstrapper {
             final var database = new SQLiteDatabase(this);
             this.plugin = new FarmWorldPlugin(this, database);
             FarmWorldAPI.setApi(new APIImpl(this.plugin));
-            loadFarmWorlds();
-
             this.plugin.enable();
         } catch (Exception e) {
             getSLF4JLogger().error("Failed to enable plugin!", e);
@@ -35,59 +27,6 @@ public class FarmWorldBootstrapper extends Bootstrapper {
             this.plugin.disable();
         } catch (Exception e) {
             getSLF4JLogger().error("Failed to disable plugin!", e);
-        }
-    }
-
-    private CompletableFuture<Void> loadFarmWorlds() {
-        return CompletableFuture.runAsync(() -> {
-            final var folder = getOrCreateWorldsFolder();
-            getSLF4JLogger().info("Loading farm worlds in folder {}...", folder.getPath());
-            createDefaultFarmWorld(folder);
-
-            for (final var file : folder.listFiles()) {
-                if (!checkWorldsFile(file)) continue;
-
-                try {
-                    new FarmWorldLoader()
-                            .readFile(file)
-                            .loadAndRegister();
-                } catch (IOException exception) {
-                    getSLF4JLogger().error("Failed to read file {}", file.getPath(), exception);
-                }
-            }
-        });
-    }
-
-    private File getOrCreateWorldsFolder() {
-        if (!getDataFolder().exists()) getDataFolder().mkdirs();
-        final var folder = new File(getDataFolder(), "worlds");
-        if (!folder.exists()) folder.mkdirs();
-        return folder;
-    }
-
-    private boolean checkWorldsFile(File file) {
-        if (file.isDirectory()) {
-            getSLF4JLogger().warn("Folder {} is not a json file!", file.getPath());
-            return false;
-        }
-
-        if (!file.getName().endsWith(".json")) {
-            getSLF4JLogger().warn("File {} is not a json file!", file.getPath());
-            return false;
-        }
-        return true;
-    }
-
-    private void createDefaultFarmWorld(File folder) {
-        if (folder.listFiles() == null) throw new NullPointerException("Failed to load farm worlds!");
-        if (folder.listFiles().length != 0) return;
-        try {
-            getSLF4JLogger().warn("No farm worlds found!");
-            getSLF4JLogger().info("Creating default farm world.");
-            new FarmWorldConfig("FarmWorld", null, World.Environment.NORMAL, null, false)
-                    .save(new File(folder, "farm_world.json"));
-        } catch (IOException exception) {
-            getSLF4JLogger().error("Failed to create default farm world!", exception);
         }
     }
 }
--- paper-plugin/src/main/java/at/mrcl/farmworld/FarmWorldPlugin.java
+++ paper-plugin/src/main/java/at/mrcl/farmworld/FarmWorldPlugin.java
@@ -4,8 +4,13 @@ import at.mrcl.farmworld.api.Bootstrapper;
 import at.mrcl.farmworld.api.FarmWorld;
 import at.mrcl.farmworld.api.database.Database;
 import lombok.Getter;
+import org.bukkit.World;
+import org.slf4j.Logger;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -26,11 +31,73 @@ public class FarmWorldPlugin {
     public void enable() throws Exception {
         if (!this.enabled.compareAndSet(false, true)) throw new IllegalStateException("Plugin is already enabled!");
         this.database.connect();
+        loadFarmWorlds();
     }
 
     public void disable() throws Exception {
         if (!this.enabled.compareAndSet(true, false)) throw new IllegalStateException("Plugin is already disabled!");
         this.database.disconnect();
+        this.farmWorlds.clear();
     }
 
+    public Logger getLogger() {
+        return this.bootstrapper.getSLF4JLogger();
+    }
+
+    public File getDataFolder() {
+        return this.bootstrapper.getDataFolder();
+    }
+
+    private CompletableFuture<Void> loadFarmWorlds() {
+        return CompletableFuture.runAsync(() -> {
+            final var folder = getOrCreateWorldsFolder();
+            getLogger().info("Loading farm worlds in folder {}...", folder.getPath());
+            createDefaultFarmWorld(folder);
+
+            for (final var file : folder.listFiles()) {
+                if (!checkWorldsFile(file)) continue;
+
+                try {
+                    new FarmWorldLoader()
+                            .readFile(file)
+                            .loadAndRegister();
+                } catch (IOException exception) {
+                    getLogger().error("Failed to read file {}", file.getPath(), exception);
+                }
+            }
+        });
+    }
+
+    private File getOrCreateWorldsFolder() {
+        if (!getDataFolder().exists()) getDataFolder().mkdirs();
+        final var folder = new File(getDataFolder(), "worlds");
+        if (!folder.exists()) folder.mkdirs();
+        return folder;
+    }
+
+    private boolean checkWorldsFile(File file) {
+        if (file.isDirectory()) {
+            getLogger().warn("Folder {} is not a json file!", file.getPath());
+            return false;
+        }
+
+        if (!file.getName().endsWith(".json")) {
+            getLogger().warn("File {} is not a json file!", file.getPath());
+            return false;
+        }
+        return true;
+    }
+
+    private void createDefaultFarmWorld(File folder) {
+        if (folder.listFiles() == null) throw new NullPointerException("Failed to load farm worlds!");
+        if (folder.listFiles().length != 0) return;
+        try {
+            getLogger().warn("No farm worlds found!");
+            getLogger().info("Creating default farm world.");
+            new FarmWorldConfig("FarmWorld", null, World.Environment.NORMAL, null, false)
+                    .save(new File(folder, "farm_world.json"));
+        } catch (IOException exception) {
+            getLogger().error("Failed to create default farm world!", exception);
+        }
+    }
 }
